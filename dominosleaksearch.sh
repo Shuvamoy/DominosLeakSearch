d13b370dbe23779c98f4c3b4ae40b86a38eec556
Shuvamoy Roy <bosepriyojit8@gmail.com>
2021-05-24 03:19:03 +0530

    Update dominosleaksearch.sh

--- dominosleaksearch.sh
+++ dominosleaksearch.sh
@@ -1,8 +1,8 @@
 echo ""; echo "";
-echo "==============================="
-echo "Easy Dominos Leak Data Searcher"
-echo "==============================="
-echo ""; echo "www.abnersecurity.com"; echo ""; echo "Enter your search query: (your email/cell no)"; echo "";
+echo "\e[1;31m===============================\e[0m"
+echo "\e[1;47m\e[1;44mEasy Dominos Leak Data Searcher\e[0m"
+echo "\e[1;31m===============================\e[0m"
+echo ""; echo "\e[1;33mwww.abnersecurity.com\e[0m"; echo ""; echo "\e[1;31mEnter your search query: (your email/cell no)\e[0m"; echo "";
 read query
 curl -s http://slf2rrahypck3bwckpdohsnhpeqrb3nhvwznjmarmweofwnptowe4mad.onion.ly/api/search/$query -o file.txt ;
 cat file.txt  | python -m json.tool;
